48c4ab78b2d82e678363f4a8ef01d1a1e4824817
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-01 20:26:16 +0600

    Fibonacci Series added

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Fibonacci_Series {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println();
+        System.out.println("Enter the value of n: ");
+        int n = s.nextInt();
+        int a = 0, b = 1;
+        System.out.println("Fibonacci Series till first 10 terms: ");
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(a + " ");
+            int f = a + b;
+            a = b;
+            b = f;
+
+        }
         s.close();
     }
 }
