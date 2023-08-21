2234e8862a6beae4c8cb75573c25afabaac2f343
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-21 06:36:23 +0600

    multilevel inheritance added

--- MultiLevel_inheritance.java
+++ MultiLevel_inheritance.java
@@ -1,25 +1,33 @@
-class supers {
+class tri {
     public void triangle(int x, int y) {
         double a = 0.5 * x * y;
         System.out.println("Area Of triangle= " + a);
     }
 }
 
-class Sub extends Super {
+class rec extends tri {
     public void rectangle(int x, int y) {
         int b = x * y;
         System.out.println("Area Of rectangle= " + b);
+    }
+}
+
+class add extends rec {
+    public void addiction(int x, int y) {
+        int c = x * y;
+        System.out.println("Addition Of Two Number= " + c);
     }
 
     public void display() {
         triangle(10, 2);
         rectangle(20, 2);
+        addiction(5, 10);
     }
 }
 
-public class Super {
+public class MultiLevel_inheritance {
     public static void main(String[] args) {
-        Sub s = new Sub();
+        add s = new add();
         s.display();
     }
 }
